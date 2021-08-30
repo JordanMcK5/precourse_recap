168798f971562b592fa27bb2df9c159bc63809a5
Jordan McKenna <jordanmckenna@JORDANs-MacBook-Pro.local>
2021-08-30 17:01:46 +0100

    First homework commit

---
+++ precourse_recap.py
@@ -0,0 +1,7 @@
+campus = input ("where are the G27 cohort based?")
+if campus == "glasgow":
+    print("Correct, Glasgow Yay!")
+elif campus == "edinburgh":
+    print("No but Codeclan do have a campus in the Capital!")
+else:
+    print("sorry, it's not there, guess again!")
